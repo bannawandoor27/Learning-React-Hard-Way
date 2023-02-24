d031d0ceba20788b897491a3ddfcf45e4cf2b53d
hasanul banna <bannawandoor@gmail.com>
2023-02-24 10:28:37 +0530

    js tests

---
+++ src/test.js
@@ -0,0 +1,87 @@
+
+// var arr = ['apple', 'orange',
+//  'grape', 'banana','mango','ghoosberry','mulberry','strawberry'];
+var True = false
+// var [first, ...last] =arr
+
+// console.log(first);
+// var xy = Object()
+// xy.key = 'value'
+
+// console.log(xy);
+
+// var map1 = new Map([
+//     [1, 2],
+//     [2, 3],
+//     [4, 'banan']
+// ]);
+
+// var map3 = new Map([
+//     ["whole numbers", [1, 2, 3, 4]],
+//     ["Decimal numbers", [1.1, 1.2, 1.3, 1.4]],
+//     ["negative numbers", [-1, -2, -3, -4]]
+// ]
+// )
+// map1.set('value', 'apple');
+// console.log("Map3");
+// console.log(map3.entries());
+
+// var map4 = new Map([
+//     [
+//         ["first name", "last name"],
+//         ["sumit", "ghosh"]
+//     ],
+//     [
+//         ["friend 1", "friend 2"],
+//         ["sourav", "gourav"]
+//     ]
+// ]);
+ 
+// console.log("Map4");
+
+// class Banna{
+//     constructor (name){
+//         this.name = name;
+//         this.fullName = this.name + "mon";
+//     }
+
+//     printFullName(){
+//         console.log(this.fullName);
+//     }
+//     banna = function(){
+//         console.log('jaba jaba');
+//     }
+// }
+// var banna1 = new Banna("banna1");
+
+// banna1.banna()
+var jaba = new Map()
+jaba.set('name', 'jaba')
+jaba.set('age', 20)
+jaba.set('hobby', 'music')
+jaba.set('hobby', 'sports')
+
+
+// var banna=()=>{
+//     console.log(jaba.values())
+// }
+// banna();
+
+let husnu=(map)=>{
+    return arr = map.values();
+}
+// console.log([...husnu(jaba)]);
+
+
+var arrr= [...husnu(jaba)];
+function husnu1(element){ 
+    element = element+'fsdf';
+    
+    return element;
+}
+// console.log(arrr.map(husnu1));
+
+var arr1 = [1,2,3,4,5,6,7,8,9,]
+
+
+
